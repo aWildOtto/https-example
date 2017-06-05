80fc97ec0fcaca6165fe04fad1d66cc72f89fe38
otto hu <ottohu101@gmail.com>
2017-06-05 07:32:40 +0000

    step 7

--- https_example.js
+++ https_example.js
@@ -1,9 +1,9 @@
 var https = require('https');
 
-
+var i = 0;
 var options = {
-  host: 'www.example.org',
-  path: '/'
+  host: 'www.theverge.com',
+  path: '/2017/6/5/15739136/apple-files-app-ios-11-wwdc-2017'
 };
 
 // called by https when the request is made.
@@ -12,6 +12,7 @@ var callback = function(response) {
 
   response.on('data', function(chunk) {
     console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    i++;
     console.log(chunk.toString());
   });
 }
@@ -21,3 +22,5 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
+console.log(i+" chunks recieved");
